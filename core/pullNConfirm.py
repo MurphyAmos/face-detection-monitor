71476b46b631f67a95c5ffc30e7fd5bcc74c2f41
Murphy Amos <46387288+MurphyAmos@users.noreply.github.com>
2026-05-08 16:43:58 -0500

    Update pullNConfirm.py

--- core/pullNConfirm.py
+++ core/pullNConfirm.py
@@ -5,15 +5,15 @@ def pullVideo():
         # Define the bounding box for the screen capture. The bounding box is defined by its top-left corner (top, left) and its width and height. 
         bounding_box = {'top': 200, 'left': 0, 'width': 900, 'height': 800}
         sct = mss()
-        fc = 0
-        fs = 25
+        #fc = 0
+        #fs = 25
         while True:
             # Capture the screen within the defined bounding box and convert it to a format suitable for face recognition. 
             # The captured screen is converted from BGR color space (used by OpenCV) to RGB color space (used by face_recognition) to ensure compatibility with the face recognition library. 
             windowFrame = cv2.cvtColor(np.array(sct.grab(bounding_box)), cv2.COLOR_BGR2RGB)
-            if fc % fs == 0:
-                face_locations = face_recognition.face_locations(windowFrame)     
-            fc += 1
+            #if fc % fs == 0:
+            face_locations = face_recognition.face_locations(windowFrame)     
+            #fc += 1
             # If face locations are found, we call the compareFaces function to compare the detected faces with known encodings. The results of the comparison are printed for debugging purposes. 
             # This allows us to continuously check for new faces in subsequent frames without retaining old face location data.
             if face_locations:
@@ -26,6 +26,5 @@ def pullVideo():
             else:
                 # If no faces are detected, a message is printed to indicate that no faces were found in the current frame. 
                 print(f"\r Face comparison result: No face found           ",end=" ",flush=True)
-            fc = 0
 if __name__ == "__main__":
     pullVideo()
